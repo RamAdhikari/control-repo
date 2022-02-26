e51abea44157eba8e4b854239b6431c9073467db
Ram Adhikari <im_ram@hotmail.com>
2022-02-26 20:51:17 +1100

    updated file path

--- site-modules/profile/manifests/windows/aclmachinekeys.pp
+++ site-modules/profile/manifests/windows/aclmachinekeys.pp
@@ -1,7 +1,7 @@
 class profile::windows::aclmachinekeys{
 
 exec { 'machinekayacl':
-  command   => file('windows/restore-machinekeys-acl.ps1'),
+  command   => file('profile/restore-machinekeys-acl.ps1'),
   unless    =>'if(((((((get-acl C:\\ProgramData\\Microsoft\\Crypto\\RSA\\MachineKeys).access)| select IdentityReference | ft -HideTableHeaders | Out-String).trim()) -replace "\`r\`n", "" ) -replace "              ",",") -ne "Everyone,BUILTIN\Administrators"){exit 1 }',
   provider  => powershell,
   }
