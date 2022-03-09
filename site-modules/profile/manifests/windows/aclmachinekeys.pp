5eb32caea8c1b0ee248914f9f2f5d29c2e62a5e8
Ram Adhikari <im_ram@hotmail.com>
2022-03-09 18:12:08 +1100

    updated if condition logic

--- site-modules/profile/manifests/windows/aclmachinekeys.pp
+++ site-modules/profile/manifests/windows/aclmachinekeys.pp
@@ -2,7 +2,7 @@ class profile::windows::aclmachinekeys{
 
 exec { 'machinekayacl':
   command   => file('profile/restore-machinekeys-acl.ps1'),
-  unless    =>'if(((Get-Acl C:\\Programdata\\Microsoft\\Crypto\\RSA\\MachineKeys |select-object -ExpandProperty sddl|Out-String).length) -gt 45)){exit 1 }',
+  unless    =>'if(((Get-Acl "C:\\Programdata\\Microsoft\\Crypto\\RSA\\MachineKeys" |select-object -ExpandProperty sddl|Out-String).length) -gt 45){exit 1 }',
   provider  => powershell,
   }
 
